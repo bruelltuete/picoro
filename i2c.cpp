7f96733b28b83bc7a8272487aa4ef7142295c303
jo <jo@bruelltuete.com>
2022-05-12 16:11:30 +0100

    sanitise some stuff in the i2c driver

--- i2c.cpp
+++ i2c.cpp
@@ -8,17 +8,9 @@
 #include "ringbuffer.h"
 
 
-static int              dmaread[2] = {-1, -1};
-static int              dmawrite[2] = {-1, -1};
-static const int        dmairq[2] = {0, 0};
-static volatile bool    datareadcaused[2];
-static volatile bool    datawritecaused[2];
+static const int        dmairq[2] = {1, 1};
 
-
-// FIXME: do we want one coro per i2c controller? or one for both controllers?
-static Coroutine<256>      i2cdriverblock[2];
-
-static struct CmdRingbufferEntry
+struct CmdRingbufferEntry
 {
     int                 numcmds;
     uint16_t*           cmds;
@@ -26,26 +18,76 @@ static struct CmdRingbufferEntry
     uint8_t*            results;
     Waitable            waitable;
     int8_t              address;
-}                   cmdringbuffer[2][RINGBUFFER_SIZE];
-static RingBuffer   cmdindices[2];
-static Waitable     newcmdswaitable[2];
+};
+
+static struct DriverState
+{
+    int                 dmareadchannel;
+    int                 dmawritechannel;
+
+    RingBuffer          cmdindices;
+    Coroutine<256>      i2cdriverblock;
+    Waitable            newcmdswaitable;
+
+    CmdRingbufferEntry  cmdringbuffer[RINGBUFFER_SIZE];
+
+    volatile bool       datareadcaused;
+    volatile bool       datawritecaused;
+    volatile bool       drivershouldexit;
+
+    DriverState() : dmareadchannel(-1) {}
+}           driverstate[2];
 
 
 template <int I>
 static void dma_irq_handler()
 {
-    bool r = dma_irqn_get_channel_status(dmairq[I], dmaread[I]);
-    bool w = dma_irqn_get_channel_status(dmairq[I], dmawrite[I]);
+    uint ex = __get_current_exception();
+    assert((ex - 16) == (DMA_IRQ_0 + dmairq[I]));
+
+    // FIXME: workaround for bug in pico-sdk. cannot remove and re-add a shared handler
+    //        so we leave ours here dangling and deal with that fact.
+    if (driverstate[I].dmareadchannel == -1)
+        return;
+
+    bool r = dma_irqn_get_channel_status(dmairq[I], driverstate[I].dmareadchannel);
+    bool w = dma_irqn_get_channel_status(dmairq[I], driverstate[I].dmawritechannel);
     if (r)
-        dma_irqn_acknowledge_channel(dmairq[I], dmaread[I]);
+        dma_irqn_acknowledge_channel(dmairq[I], driverstate[I].dmareadchannel);
     if (w)
-        dma_irqn_acknowledge_channel(dmairq[I], dmawrite[I]);
+        dma_irqn_acknowledge_channel(dmairq[I], driverstate[I].dmawritechannel);
 
-    datareadcaused[I] |= r;
-    datawritecaused[I] |= w;
+    driverstate[I].datareadcaused |= r;
+    driverstate[I].datawritecaused |= w;
 
-    if (datawritecaused[I] && datareadcaused[I])
-        wakeup(&i2cdriverblock[I]);
+    if (driverstate[I].datawritecaused && driverstate[I].datareadcaused)
+    {
+        wakeup(&driverstate[I].i2cdriverblock);
+        // reset ack flags "after" handling. so that we trigger this only once!
+        driverstate[I].datareadcaused = false;
+        driverstate[I].datawritecaused = false;
+    }
+}
+
+static const irq_handler_t dmahandlers[] = {dma_irq_handler<0>, dma_irq_handler<1>};
+
+static void deinit(int i2cindex)
+{
+    // should only be called once all queued up cmds have been drained.
+    assert(rb_is_empty(&driverstate[i2cindex].cmdindices));
+
+    dma_irqn_set_channel_enabled(dmairq[i2cindex], driverstate[i2cindex].dmareadchannel, false);
+    dma_irqn_set_channel_enabled(dmairq[i2cindex], driverstate[i2cindex].dmawritechannel, false);
+    
+    // FIXME: pico-sdk has a bug in add/remove shared handler.
+    //        so we'll never remove our handlers here and make sure to add them only once in init().
+    //        and also the handler can deal with being called when it should have been registered.
+    //irq_remove_handler(DMA_IRQ_0 + dmairq[i2cindex], dmahandlers[i2cindex]);
+
+    dma_channel_unclaim(driverstate[i2cindex].dmareadchannel);
+    driverstate[i2cindex].dmareadchannel = -1;
+    dma_channel_unclaim(driverstate[i2cindex].dmawritechannel);
+    driverstate[i2cindex].dmawritechannel = -1;
 }
 
 static uint32_t i2cdriver_func(uint32_t param)
@@ -55,18 +97,18 @@ static uint32_t i2cdriver_func(uint32_t param)
 
     while (true)
     {
-        if (!rb_is_empty(&cmdindices[i2cindex]))
+        if (!rb_is_empty(&driverstate[i2cindex].cmdindices))
         {
             // FIXME: previous dma might still be in progress!
             //        actually, it shouldnt... but might be worth asserting on.
 
             // not empty, so do the thing.
-            CmdRingbufferEntry&  c = cmdringbuffer[i2cindex][rb_peek_front(&cmdindices[i2cindex])];
+            CmdRingbufferEntry&  c = driverstate[i2cindex].cmdringbuffer[rb_peek_front(&driverstate[i2cindex].cmdindices)];
 
             // we will always have to do some writes.
-            datawritecaused[i2cindex] = false;
+            driverstate[i2cindex].datawritecaused = false;
             // but reads are optional. so if there are no reads then we pretend the read dma has finished already.
-            datareadcaused[i2cindex] = c.numresults == 0;
+            driverstate[i2cindex].datareadcaused = c.numresults == 0;
 
             i2c->hw->enable = 0;
             i2c->hw->tar = c.address;
@@ -74,15 +116,15 @@ static uint32_t i2cdriver_func(uint32_t param)
 
             if (c.numresults > 0)
             {
-                dma_channel_set_read_addr(dmaread[i2cindex], &i2c->hw->data_cmd, false);
-                dma_channel_set_write_addr(dmaread[i2cindex], &c.results[0], false);
-                dma_channel_set_trans_count(dmaread[i2cindex], c.numresults, false);
-                dma_channel_start(dmaread[i2cindex]);
+                dma_channel_set_read_addr(driverstate[i2cindex].dmareadchannel, &i2c->hw->data_cmd, false);
+                dma_channel_set_write_addr(driverstate[i2cindex].dmareadchannel, &c.results[0], false);
+                dma_channel_set_trans_count(driverstate[i2cindex].dmareadchannel, c.numresults, false);
+                dma_channel_start(driverstate[i2cindex].dmareadchannel);
             }
-            dma_channel_set_read_addr(dmawrite[i2cindex], &c.cmds[0], false);
-            dma_channel_set_write_addr(dmawrite[i2cindex], &i2c->hw->data_cmd, false);
-            dma_channel_set_trans_count(dmawrite[i2cindex], c.numcmds, false);
-            dma_channel_start(dmawrite[i2cindex]);
+            dma_channel_set_read_addr(driverstate[i2cindex].dmawritechannel, &c.cmds[0], false);
+            dma_channel_set_write_addr(driverstate[i2cindex].dmawritechannel, &i2c->hw->data_cmd, false);
+            dma_channel_set_trans_count(driverstate[i2cindex].dmawritechannel, c.numcmds, false);
+            dma_channel_start(driverstate[i2cindex].dmawritechannel);
 
             yield_and_wait4wakeup();
 
@@ -94,19 +136,24 @@ static uint32_t i2cdriver_func(uint32_t param)
 #endif
             signal(&c.waitable);
 
-            rb_pop_front(&cmdindices[i2cindex]);
+            rb_pop_front(&driverstate[i2cindex].cmdindices);
         }
 
-        yield_and_wait4signal(&newcmdswaitable[i2cindex]);
+        // FIXME: i think i need a timeout-able wait4signal, so that the driver can shutdown.
+        yield_and_wait4signal(&driverstate[i2cindex].newcmdswaitable);
+        if (driverstate[i2cindex].drivershouldexit && rb_is_empty(&driverstate[i2cindex].cmdindices))
+            break;
 
 #ifndef NDEBUG
         {
             // if we've been signaled that there's a new cmd than there better be a new command!
-            CmdRingbufferEntry&  c = cmdringbuffer[i2cindex][rb_peek_front(&cmdindices[i2cindex])];
+            CmdRingbufferEntry&  c = driverstate[i2cindex].cmdringbuffer[rb_peek_front(&driverstate[i2cindex].cmdindices)];
             assert(c.numcmds > 0);
         }
 #endif
     }
+
+    deinit(i2cindex);
     return 0;
 }
 
@@ -124,21 +171,21 @@ static i2c_inst_t* i2cinst_from_index(int index)
 
 static void init(int i2cindex)
 {
-    if (dmaread[i2cindex] != -1)
+    if (driverstate[i2cindex].dmareadchannel != -1)
         return;
 
-    static const irq_handler_t dmahandlers[] = {dma_irq_handler<0>, dma_irq_handler<1>};
-
-    rb_init_ringbuffer(&cmdindices[i2cindex]);
-    memset(&newcmdswaitable[i2cindex], 0, sizeof(newcmdswaitable[i2cindex]));
+    driverstate[i2cindex].drivershouldexit = false;
+    rb_init_ringbuffer(&driverstate[i2cindex].cmdindices);
+    memset(&driverstate[i2cindex].newcmdswaitable, 0, sizeof(driverstate[i2cindex].newcmdswaitable));
+    memset(&driverstate[i2cindex].i2cdriverblock, 0, sizeof(driverstate[i2cindex].i2cdriverblock));
 
     // side note: caller should have called i2c_init(), which enables transmit/receive dreq on the i2c side.
     i2c_inst_t* i2c = i2cinst_from_index(i2cindex);
 
     // dma channel for writing data to device
     {
-        dmawrite[i2cindex] = dma_claim_unused_channel(true);
-        dma_channel_config dmawritecfg = dma_channel_get_default_config(dmawrite[i2cindex]);
+        driverstate[i2cindex].dmawritechannel = dma_claim_unused_channel(true);
+        dma_channel_config dmawritecfg = dma_channel_get_default_config(driverstate[i2cindex].dmawritechannel);
         // we are reading from a cpu array and writing it to the same i2c address
         channel_config_set_read_increment(&dmawritecfg, true);
         channel_config_set_write_increment(&dmawritecfg, false);
@@ -146,29 +193,36 @@ static void init(int i2cindex)
         // i2c transmits bytes as data, but the controller receives control bits in the higher bits of the word
         channel_config_set_transfer_data_size(&dmawritecfg, DMA_SIZE_16);
         channel_config_set_dreq(&dmawritecfg, i2c_get_dreq(i2c, true));
-        dma_channel_set_config(dmawrite[i2cindex], &dmawritecfg, false);
+        dma_channel_set_config(driverstate[i2cindex].dmawritechannel, &dmawritecfg, false);
 
-        dma_irqn_set_channel_enabled(dmairq[i2cindex], dmawrite[i2cindex], true);
+        dma_irqn_set_channel_enabled(dmairq[i2cindex], driverstate[i2cindex].dmawritechannel, true);
     }
 
     // dma channel for reading bytes coming from the chip.
     {
-        dmaread[i2cindex] = dma_claim_unused_channel(true);
-        dma_channel_config dmareadcfg = dma_channel_get_default_config(dmaread[i2cindex]);
+        driverstate[i2cindex].dmareadchannel = dma_claim_unused_channel(true);
+        dma_channel_config dmareadcfg = dma_channel_get_default_config(driverstate[i2cindex].dmareadchannel);
         channel_config_set_read_increment(&dmareadcfg, false);  // always reading from i2c fifo register
         channel_config_set_write_increment(&dmareadcfg, true);  // writing sequentially into system mem
 
         channel_config_set_transfer_data_size(&dmareadcfg, DMA_SIZE_8);
         channel_config_set_dreq(&dmareadcfg, i2c_get_dreq(i2c, false));
-        dma_channel_set_config(dmaread[i2cindex], &dmareadcfg, false);
+        dma_channel_set_config(driverstate[i2cindex].dmareadchannel, &dmareadcfg, false);
         
-        dma_irqn_set_channel_enabled(dmairq[i2cindex], dmaread[i2cindex], true);
+        dma_irqn_set_channel_enabled(dmairq[i2cindex], driverstate[i2cindex].dmareadchannel, true);
     }
 
-    irq_add_shared_handler(DMA_IRQ_0 + dmairq[i2cindex], dmahandlers[i2cindex], PICO_SHARED_IRQ_HANDLER_DEFAULT_ORDER_PRIORITY);
+    // FIXME: workaround for bug in pico-sdk. remove and re-adding a shared handler does not work.
+    //        so make sure we add our dma handler only once and never remove it again.
+    static bool alreadyadded[2] = {false, false};
+    if (!alreadyadded[i2cindex])
+    {
+        irq_add_shared_handler(DMA_IRQ_0 + dmairq[i2cindex], dmahandlers[i2cindex], PICO_SHARED_IRQ_HANDLER_DEFAULT_ORDER_PRIORITY);
+        alreadyadded[i2cindex] = true;
+    }
     irq_set_enabled(DMA_IRQ_0 + dmairq[i2cindex], true);
 
-    yield_and_start(i2cdriver_func, (uint32_t) i2c, &i2cdriverblock[i2cindex]);
+    yield_and_start(i2cdriver_func, (uint32_t) i2c, &driverstate[i2cindex].i2cdriverblock);
 }
 
 Waitable* queue_cmds(i2c_inst_t* i2c, int8_t address, int numcmds, uint16_t* cmds, int numresults, uint8_t* results)
@@ -181,14 +235,14 @@ Waitable* queue_cmds(i2c_inst_t* i2c, int8_t address, int numcmds, uint16_t* cmd
 
     init(i2cindex);
 
-    while (rb_is_full(&cmdindices[i2cindex]))
+    while (rb_is_full(&driverstate[i2cindex].cmdindices))
     {
         // if cmd buffer is full then wait for current transfer to finish.
         // note that this is a loop because there might be another coro waiting for this transfer already and it might be woken sooner than us and queue another transfer in front of us.
-        yield_and_wait4signal(&cmdringbuffer[i2cindex][rb_peek_front(&cmdindices[i2cindex])].waitable);
+        yield_and_wait4signal(&driverstate[i2cindex].cmdringbuffer[rb_peek_front(&driverstate[i2cindex].cmdindices)].waitable);
     }
 
-    CmdRingbufferEntry&  c = cmdringbuffer[i2cindex][rb_push_back(&cmdindices[i2cindex])];
+    CmdRingbufferEntry&  c = driverstate[i2cindex].cmdringbuffer[rb_push_back(&driverstate[i2cindex].cmdindices)];
     c.numcmds = numcmds;
     c.cmds = cmds;
     c.numresults = numresults;
@@ -196,7 +250,32 @@ Waitable* queue_cmds(i2c_inst_t* i2c, int8_t address, int numcmds, uint16_t* cmd
     c.address = address;
 
     // tell driver that there are new cmds waiting.
-    signal(&newcmdswaitable[i2cindex]);
+    signal(&driverstate[i2cindex].newcmdswaitable);
 
     return &c.waitable;
+}
+
+const CoroutineHeader* get_driver_coro(i2c_inst_t* i2c)
+{
+    const int i2cindex = i2c_hw_index(i2c);
+
+    if (driverstate[i2cindex].dmareadchannel == -1)
+        return NULL;
+
+    return &driverstate[i2cindex].i2cdriverblock;
+}
+
+void stop_i2c_driver_async()
+{
+    if (driverstate[0].dmareadchannel != -1)
+    {
+        driverstate[0].drivershouldexit = true;
+        signal(&driverstate[0].newcmdswaitable);
+    }
+
+    if (driverstate[1].dmareadchannel != -1)
+    {
+        driverstate[1].drivershouldexit = true;
+        signal(&driverstate[1].newcmdswaitable);
+    }
 }
--- i2c.h
+++ i2c.h
@@ -9,4 +9,12 @@
 // does not itself yield internally, unless internal ringbuffer is full.
 extern Waitable* queue_cmds(i2c_inst_t* i2c, int8_t address, int numcmds, uint16_t* cmds, int numresults, uint8_t* results);
 
-// FIXME: i need an explicit init()/deinit() so that the main app and shut everything down.
+// FIXME: i need an explicit init()/deinit() so that the main app can shut everything down.
+
+
+// can return NULL if there is no driver yet for that i2c instance!
+// maybe only useful for debugging.
+extern const CoroutineHeader* get_driver_coro(i2c_inst_t* i2c);
+
+// tells the drivers (both!) to stop, usually after their current commands have drained.
+extern void stop_i2c_driver_async();
